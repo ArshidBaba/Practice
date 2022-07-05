b59108cbba5410e5ce60258b89e5aad7b186d926
Arshid Baba <arshidbaba93@gmail.com>
2022-07-05 22:14:31 +0530

    Learned Relationships and hyperlinked APIs

--- practice/snippets/views.py
+++ practice/snippets/views.py
@@ -11,6 +11,24 @@ from rest_framework import mixins
 from rest_framework import generics
 from django.contrib.auth.models import User
 from rest_framework import permissions
+from rest_framework import reverse
+from rest_framework import renderers
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
 
 
 class SnippetList(generics.ListCreateAPIView):
