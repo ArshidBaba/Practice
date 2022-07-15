c0508592fa00a0f095cc4eaedf9b751415778c4e
Arshid Baba <arshidbaba93@gmail.com>
2022-07-15 12:44:40 +0530

    created an apis app

--- practice/practice/urls.py
+++ practice/practice/urls.py
@@ -17,12 +17,14 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     # path("", include('api.api_urls')),
-    path("", include('snippets.urls')),
-    path('album/', include('api.api_urls'))
+    path("", include("snippets.urls")),
+    path("album/", include("api.api_urls")),
+    path("search/", include("search.urls")),
+    path("books/", include("books.urls")),
 ]
 
 urlpatterns += [
-    path('api-auth/', include('rest_framework.urls')),
+    path("api-auth/", include("rest_framework.urls")),
 ]
