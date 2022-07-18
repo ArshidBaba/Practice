76e9a21906bfc9ad3c24f2076ae5d6ed4a9dab57
Arshid Baba <arshidbaba93@gmail.com>
2022-07-18 14:33:48 +0530

    Class Methods and attributes

--- practice/oop/main.py
+++ practice/oop/main.py
@@ -13,15 +13,25 @@ class Item:
     def calculate_total_price(self):
         return self.price * self.quantity
 
+    def apply_discount(self):
+        self.price = self.price * self.pay_rate
+
 
 item1 = Item("Phone", 100, 2)
 item2 = Item("Laptop", 1000, 3)
+item1.apply_discount()
 
+item2.pay_rate = 0.7
+print(item1.price)
+
+# item2.pay_rate = 0.7
+item2.apply_discount()
+print(item2.price)
 # print(item1.calculate_total_price())
 # print(item2.calculate_total_price())
 # print(Item.pay_rate)
 # print(item1.pay_rate)
 # print(item2.pay_rate)
 
-print(Item.__dict__)
-print(item1.__dict__)
+# print(Item.__dict__)
+# print(item1.__dict__)
