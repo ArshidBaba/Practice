0e5fe647d41c5d7331708c9c962d37ad00ad8530
Arshid Baba <arshidbaba93@gmail.com>
2022-07-18 14:49:00 +0530

    Representation of objects

--- practice/oop/main.py
+++ practice/oop/main.py
@@ -1,14 +1,17 @@
 class Item:
     pay_rate = 0.8
+    all = []
 
     def __init__(self, name: str, price: float, quantity=0):
         # print(f"An instance created: {name}")
         assert price >= 0, f"Price {price} is not greater than zero!"
-        assert quantity >= 0
+        assert quantity >= 0, f"Quantity {quantity} is not greater than zero!"
 
         self.name = name
         self.price = price
         self.quantity = quantity
+
+        Item.all.append(self)
 
     def calculate_total_price(self):
         return self.price * self.quantity
@@ -16,22 +19,17 @@ class Item:
     def apply_discount(self):
         self.price = self.price * self.pay_rate
 
+    def __repr__(self):
+        return f"Item('Name: {self.name}', 'Price: {self.price}', 'Quantity: {self.quantity}')"
 
-item1 = Item("Phone", 100, 2)
+
+item1 = Item("Phone", 100, 1)
 item2 = Item("Laptop", 1000, 3)
-item1.apply_discount()
+item3 = Item("Cable", 10, 5)
+item4 = Item("Mouse", 50, 5)
+item5 = Item("Keyboard", 75, 5)
 
-item2.pay_rate = 0.7
-print(item1.price)
+print(Item.all)
 
-# item2.pay_rate = 0.7
-item2.apply_discount()
-print(item2.price)
-# print(item1.calculate_total_price())
-# print(item2.calculate_total_price())
-# print(Item.pay_rate)
-# print(item1.pay_rate)
-# print(item2.pay_rate)
-
-# print(Item.__dict__)
-# print(item1.__dict__)
+# for instance in Item.all:
+#     print(instance.name)
