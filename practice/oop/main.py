0259e9a8f2e81323a246bf98c777b65f97f0f86a
Arshid Baba <arshidbaba93@gmail.com>
2022-07-19 12:31:28 +0530

    Polymorphism

--- practice/oop/main.py
+++ practice/oop/main.py
@@ -1,9 +1,16 @@
-from re import I
 from item import Item
 
-# from phone import Phone
+from phone import Phone
+from keyboard import Keyboard
 
-item1 = Item("MyItem", 750, 6)
-item1.name = "OtherItem"
+item1 = Keyboard("logitech", 750, 6)
 
-item1.send_email()
+item2 = Phone("iPhone12", 1000, 3)
+
+item1.apply_discount()
+
+print(item1.price)
+
+item2.apply_discount()
+
+print(item2.price)
--- practice/oop/phone.py
+++ practice/oop/phone.py
@@ -2,7 +2,7 @@ from item import Item
 
 
 class Phone(Item):
-    # all = []
+    pay_rate = 0.5
 
     def __init__(self, name: str, price: float, quantity=0, broken_phones=0):
         super().__init__(name, price, quantity)
